b93f6d27c5f271100114f45a186a44eb9c2e5bfd
AtOM18 <hardik.vardaan@gmail.com>
2025-12-30 23:36:30 +0530

    channel parsing and refactoring

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"errors"
 	"fmt"
@@ -18,37 +17,51 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not print the file %s: %s", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from file %s", inputFilePath)
 	fmt.Println("====================================================")
 
-	currentLineContents := ""
+	linesChan := getLinesChannel(f)
 
-	for {
-		buffer := make([]byte, 8) // for first 8 bytes of the file
-		n, err := f.Read(buffer)
-		if err != nil {
-			// if end of line then break the infinite loop
-			if currentLineContents != ""{
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-			if errors.Is(err, io.EOF){
-				break
-			}
-			fmt.Printf("error: %s", err.Error())
-			break
-		}
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-
-		for i := 0; i < len(parts)-1; i++{
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
-		
+	for line:= range linesChan {
+		fmt.Println("read:", line)
 	}
 
 }
+
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+
+		for{
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF){
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+
+			for i := 0; i< len(parts)-1; i++{
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return lines
+
+}
+	
